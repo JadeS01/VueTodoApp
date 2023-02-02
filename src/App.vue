b649a69f347737a5d75029d0fec397390698632b
Jade Simien <baba3of4@gmail.com>
2023-02-02 15:14:08 -0800

    color schemes

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,16 @@
 // watch is for fetching localstorage/data
 import { ref, onMounted, computed, watch } from "vue";
 const todos = ref([]);
-const name = ref('');
 const input_content = ref('');
 const input_category = ref(null);
 
 const todos_asc = computed(() => todos.value.sort((a, b) => b.createdAt - a.createdAt));
 const todos_desc = computed(() => todos.value.sort((a, b) => a.createdAt - b.createdAt));
 
+const todos_work = computed(() => todos.value.filter((e) => e.category === "work"));
+const todos_personal = computed(() => todos.value.filter((e) => e.category === "personal"));
+
 onMounted(() => {
-  name.value = localStorage.getItem('name') || "";
   todos.value = JSON.parse(localStorage.getItem("todos")) || [];
 });
 
@@ -29,7 +30,7 @@ const addTodo = () => {
   });
 
   // reset state
-  input_category.value = "";
+  input_content.value = "";
   input_category.value = null;
 };
 
@@ -37,7 +38,6 @@ const removeTodo = todo => {
   todos.value = todos.value.filter(e => e !== todo);
 };
 
-watch(name, (newVal) => { localStorage.setItem('name', newVal); });
 watch(todos, (newVal) => {
   localStorage.setItem("todos", JSON.stringify(newVal));
 }, { deep: true });
@@ -46,51 +46,89 @@ watch(todos, (newVal) => {
 
 <template>
   <main class="app">
-    <section class="greeting">
-      <h2 class="title">
-        Hello, <input type="text" placeholder="Name" v-model="name" />
-      </h2>
-    </section>
     <ThemeToggle />
-    <section class="create-todo">
-      <h3 class="">Create A Todo</h3>
+    <fieldset class="section">
+      <legend>Create A Todo</legend>
       <form @submit.prevent="addTodo">
         <h4>What are your plans?</h4>
         <input type="text" placeholder="e.g. Open a PR" v-model="input_content">
         <h4>Select a category</h4>
-        <div class="options">
-          <label for="options">
-            <input type="radio" name="category" id="category1" value="work" v-model="input_category">
-            <span class="bubble work"></span>
-            <div>Work</div>
-          </label>
-          <label for="options">
-            <input type="radio" name="category" id="category1" value="personal" v-model="input_category">
-            <span class="bubble personal"></span>
-            <div>Personal</div>
-          </label>
+        <div class="row">
+          <div class="center">
+            <label for="options" :class="`col-3 glass btn-${input_category === 'work' && 'active'}`">
+              <input type="radio" name="category" id="category1" value="work" v-model="input_category">
+              <span class="bubble work"></span>
+              <div>Work</div>
+            </label>
+            <div class="col-gap-1"></div>
+            <label for="options" :class="`col-3 glass btn-${input_category === 'personal' && 'active'}`">
+              <input type="radio" name="category" id="category1" value="personal" v-model="input_category">
+              <span class="bubble personal"></span>
+              <div>Personal</div>
+            </label>
+          </div>
+          <br />
+          <div class="center">
+            <input type="submit" value="Add todo">
+          </div>
         </div>
-        <input type="submit" value="Add todo">
       </form>
-    </section>
-    <section class="todo-list">
-      <h3>Todo List</h3>
+    </fieldset>
+    <fieldset class="section">
+      <legend>Todo Lists</legend>
+      <div class="list-row">
+
+        <fieldset class="col-6">
+          <legend>Work List</legend>
+          <div v-for="todo in todos_work" :class="`todo-item ${todo.completed && 'strike'} row`">
+            <label class="li">
+              <input type="checkbox" v-model="todo.completed">
+              <span :class="`bubble ${todo.category}`">{{ todo.category }}</span>
+            </label>
+            <div class="li">
+              <input type="text" v-model="todo.content">
+            </div>
+            <div class="li">
+              <button class="delete" @click="removeTodo(todo)">Delete</button>
+            </div>
+          </div>
+        </fieldset>
+
+        <fieldset class="col-6">
+          <legend>Personal List</legend>
+          <div v-for="todo in todos_personal" :class="`todo-item ${todo.completed && 'strike'} row`">
+            <label class="li">
+              <input type="checkbox" v-model="todo.completed">
+              <span :class="`bubble ${todo.category}`">{{ todo.category }}</span>
+            </label>
+            <div class="li">
+              <input type="text" v-model="todo.content">
+            </div>
+            <div class="li">
+              <button class="delete" @click="removeTodo(todo)">Delete</button>
+            </div>
+          </div>
+        </fieldset>
+
+      </div>
+
+
       <div class="list">
         <!-- mapping each todo -->
-        <div v-for="todo in todos_asc" :class="`todo-item ${todo.done && done}`">
-          <label>
+        <div v-for="todo in todos_asc" :class="`todo-item ${todo.completed && 'strike'} row`">
+          <label class="li">
             <input type="checkbox" v-model="todo.completed">
-            <span :class="`bubble ${todo.category}`"></span>
+            <span :class="`bubble ${todo.category}`">{{ todo.category }}</span>
           </label>
-          <div class="todo-content">
+          <div class="li">
             <input type="text" v-model="todo.content">
           </div>
-          <div class="actions">
+          <div class="li">
             <button class="delete" @click="removeTodo(todo)">Delete</button>
           </div>
         </div>
       </div>
-    </section>
+    </fieldset>
   </main>
 </template>
 
